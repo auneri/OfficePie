5322b31f76a49aafbe860950a3e386817ebc2a10
Ali Uneri <ali.uneri@jhu.edu>
2015-11-16 22:32:47 -0500

    Compute shape sizes from slide size

--- istarPresentationTemplate.py
+++ istarPresentationTemplate.py
@@ -17,9 +17,6 @@ __author__ = 'Ali Uneri'
 def main():
     p = PowerPoint()
 
-    # use widescreeen format
-    p.doc.PageSetup.SlideSize = constants.ppSlideSizeOnScreen16x9
-
     # remove unused layouts
     for layout in tuple(p.doc.SlideMaster.CustomLayouts):
         if layout.Name not in ['Title Slide', 'Title and Content', 'Section Header', 'Title Only', 'Blank']:
@@ -38,16 +35,25 @@ def main():
     p.doc.SlideMaster.Theme.ThemeColorScheme(constants.msoThemeColorAccent6).RGB = rgb(255, 255, 255)  # white
     p.doc.SlideMaster.Background.Fill.ForeColor.ObjectThemeColor = constants.msoThemeColorLight1
 
+    # use widescreeen format
+    p.doc.PageSetup.SlideSize = constants.ppSlideSizeOnScreen16x9
+    slide_height = inch(p.doc.PageSetup.SlideHeight, reverse=True)
+    slide_width = inch(p.doc.PageSetup.SlideWidth, reverse=True)
+    title_height = 1.0
+    padding = 0.4
+    margin = 0.05
+    indent = 0.4
+
     # format slide master title
     title = p.doc.SlideMaster.Shapes(1)
-    title.Left = inch(0.2)
-    title.Top = inch(0.2)
-    title.Width = inch(9.6)
-    title.Height = inch(1)
-    title.TextFrame.MarginLeft = inch(0.05)
-    title.TextFrame.MarginRight = inch(0.05)
-    title.TextFrame.MarginTop = inch(0.05)
-    title.TextFrame.MarginBottom = inch(0.05)
+    title.Left = inch(padding)
+    title.Top = inch(padding)
+    title.Width = inch(slide_width - 2 * padding)
+    title.Height = inch(title_height)
+    title.TextFrame.MarginLeft = inch(margin)
+    title.TextFrame.MarginRight = inch(margin)
+    title.TextFrame.MarginTop = inch(margin)
+    title.TextFrame.MarginBottom = inch(margin)
     title.TextFrame.TextRange.Font.Name = 'Garamond'
     title.TextFrame.TextRange.Font.Color.ObjectThemeColor = constants.msoThemeColorAccent1
     title.TextFrame.TextRange.Font.Size = 28
@@ -56,14 +62,14 @@ def main():
 
     # format slide master body
     body = p.doc.SlideMaster.Shapes(2)
-    body.Left = inch(0.2)
-    body.Top = inch(1.2)
-    body.Width = inch(9.6)
-    body.Height = inch(4.23)
-    body.TextFrame.MarginLeft = inch(0.05)
-    body.TextFrame.MarginRight = inch(0.05)
-    body.TextFrame.MarginTop = inch(0.05)
-    body.TextFrame.MarginBottom = inch(0.05)
+    body.Left = inch(padding)
+    body.Top = inch(title_height + padding)
+    body.Width = inch(slide_width - 2 * padding)
+    body.Height = inch(slide_height - 2 * padding - title_height)
+    body.TextFrame.MarginLeft = inch(margin)
+    body.TextFrame.MarginRight = inch(margin)
+    body.TextFrame.MarginTop = inch(margin)
+    body.TextFrame.MarginBottom = inch(margin)
     body.TextFrame.TextRange.Font.Name = 'Arial'
     for i in xrange(5):
         size = 18 - (2 * i)
@@ -76,10 +82,10 @@ def main():
 
     # customize text box defaults
     textbox = p.add_text('', (0,0))
-    textbox.TextFrame.MarginLeft = inch(0.05)
-    textbox.TextFrame.MarginRight = inch(0.05)
-    textbox.TextFrame.MarginTop = inch(0.05)
-    textbox.TextFrame.MarginBottom = inch(0.05)
+    textbox.TextFrame.MarginLeft = inch(margin)
+    textbox.TextFrame.MarginRight = inch(margin)
+    textbox.TextFrame.MarginTop = inch(margin)
+    textbox.TextFrame.MarginBottom = inch(margin)
     textbox.TextFrame.TextRange.Font.Name = 'Arial'
     textbox.TextFrame.TextRange.Font.Size = 12
     textbox.SetShapesDefaultProperties()
