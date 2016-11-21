5aa6626e10a1aa2295df12f7c844f0bde866fdf4
Ali Uneri <ali.uneri@jhu.edu>
2016-11-20 20:43:30 -0500

    Use qtpy in MarkRevisions to support pyside/pyqt4/5

--- MarkRevisions.py
+++ MarkRevisions.py
@@ -11,29 +11,29 @@ import argparse
 import os
 import sys
 
-from PyQt4 import QtGui
+from qtpy import QtGui, QtWidgets
 
 from PythonTools.helpers.Office import Word
 
 __author__ = 'Ali Uneri'
 
 
-class Window(QtGui.QWidget):
+class Window(QtWidgets.QWidget):
 
     def __init__(self, parent=None):
         super(Window, self).__init__(parent)
 
-        input_select = QtGui.QPushButton('...')
-        output_select = QtGui.QPushButton('...')
-        self.input_path = QtGui.QLabel('input.docx')
-        self.output_path = QtGui.QLabel(os.path.normpath(os.path.expanduser('~/Desktop/output.docx')))
-        self.strike_deletions = QtGui.QCheckBox('Strike Deletions')
-        self.progress = QtGui.QProgressBar()
-        mark = QtGui.QPushButton('Mark')
+        input_select = QtWidgets.QPushButton('...')
+        output_select = QtWidgets.QPushButton('...')
+        self.input_path = QtWidgets.QLabel('input.docx')
+        self.output_path = QtWidgets.QLabel(os.path.normpath(os.path.expanduser('~/Desktop/output.docx')))
+        self.strike_deletions = QtWidgets.QCheckBox('Strike Deletions')
+        self.progress = QtWidgets.QProgressBar()
+        mark = QtWidgets.QPushButton('Mark')
 
         self.progress.setTextVisible(False)
 
-        layout = QtGui.QGridLayout()
+        layout = QtWidgets.QGridLayout()
         layout.addWidget(input_select, 0, 0)
         layout.addWidget(self.input_path, 0, 1)
         layout.addWidget(output_select, 1, 0)
@@ -44,14 +44,14 @@ class Window(QtGui.QWidget):
         layout.setColumnStretch(1,1)
         self.setLayout(layout)
 
-        self.setAcceptDrops(True)
-        self.setAutoFillBackground(True)
-        self.setWindowTitle('Mark Revisions')
-        self.resize(0,0)
-
         input_select.clicked.connect(self.set_input)
         output_select.clicked.connect(self.set_output)
         mark.clicked.connect(self.mark)
+
+        self.setAcceptDrops(True)
+        self.setAutoFillBackground(True)
+        self.setWindowTitle('Mark Revisions')
+        self.show()
 
     def dragEnterEvent(self, event):
         if event.mimeData().urls() and event.mimeData().urls()[0].toLocalFile().endswith('.docx'):
@@ -71,12 +71,12 @@ class Window(QtGui.QWidget):
         event.accept()
 
     def set_input(self):
-        path, _ = QtGui.QFileDialog.getOpenFileName(self, 'Select input document', self.input_path.text(), 'Word Documents (*.docx)')
+        path, _ = QtWidgets.QFileDialog.getOpenFileName(self, 'Select input document', self.input_path.text(), 'Word Documents (*.docx)')
         if path:
             self.input_path.setText(os.path.normpath(path))
 
     def set_output(self):
-        path, _ = QtGui.QFileDialog.getSaveFileName(self, 'Select output document', self.output_path.text(), 'Word Documents (*.docx)')
+        path, _ = QtWidgets.QFileDialog.getSaveFileName(self, 'Select output document', self.output_path.text(), 'Word Documents (*.docx)')
         if path:
             self.output_path.setText(os.path.normpath(path))
 
@@ -104,7 +104,6 @@ if __name__ == '__main__':
             sys.stdout.flush()
         w.doc.SaveAs(args.output)
     else:
-        app = QtGui.QApplication(sys.argv)
+        app = QtWidgets.QApplication(sys.argv)
         window = Window()
-        window.show()
         sys.exit(app.exec_())
