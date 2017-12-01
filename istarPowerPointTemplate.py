65f641f0d47ba0c154c41832a43f42524f6e7515
Ali Uneri <ali.uneri@jhu.edu>
2017-11-30 19:23:10 -0500

    Streamline application to executable conversion by providing specs

--- istarPowerPointTemplate.py
+++ istarPowerPointTemplate.py
@@ -7,7 +7,7 @@
 PowerPoint template generator for I-STAR presentations.
 
 To create a portable application, run:
-    pyinstaller --clean --name=istarPowerPointTemplate --onefile --windowed --icon=istarPowerPointTemplate.ico istarPowerPointTemplate.py
+    pyinstaller istarPowerPointTemplate.spec
 
 For help in extending this template, see
 https://msdn.microsoft.com/EN-US/library/office/ee861525.aspx
